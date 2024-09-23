ebc60d0fcf9345d865444f8fa800122af2fd497d
Dominic Sky <107527331+dominicshing@users.noreply.github.com>
2024-09-23 18:03:41 +0800

    Change listen port from 8083 to 80

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ http
     res.write('A Monk in Cloud'); //write a response to the client
     res.end(); //end the response
   })
-  .listen(8083); //the server object listens on port 8083
+  .listen(80); //the server object listens on port 8083
